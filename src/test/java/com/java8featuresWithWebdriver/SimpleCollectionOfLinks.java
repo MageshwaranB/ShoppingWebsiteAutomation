ce660939a0835c4a640ebb925fc27967667262e4
Heromack <heromack10@gmail.com>
2022-05-29 14:22:36 +0530

    Updated with review comments: Added a pre java 8 example

--- src/test/java/com/java8featuresWithWebdriver/SimpleCollectionOfLinks.java
+++ src/test/java/com/java8featuresWithWebdriver/SimpleCollectionOfLinks.java
@@ -16,8 +16,16 @@ public class SimpleCollectionOfLinks {
          WebDriver driver=new ChromeDriver();
          driver.get("http://practice.automationtesting.in/");
          driver.manage().window().maximize();
+         //Stream Api example
          List<WebElement> navBarLinks=driver.findElements(By.id("main-nav")).stream().collect(Collectors.toList());
+        System.out.println("Java 8 Stream Api");
          navBarLinks.forEach(links-> System.out.println(links.getText()));
+         //Normal for each loop execution
+         navBarLinks=driver.findElements(By.id("main-nav"));
+        System.out.println("Pre Java 8 Approach");
+        for (WebElement link:navBarLinks) {
+            System.out.println(link.getText());
+        };
          driver.quit();
     }
 }
